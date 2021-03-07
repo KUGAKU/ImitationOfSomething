3e57e30671cd957348ebb9d621757adfe409c61e
gaku yamamoto <githubgemini1124@gmail.com>
2021-03-07 14:47:53 +0900

    implement tabvar

--- ImitationOfSomething/Main/MainTabView.swift
+++ ImitationOfSomething/Main/MainTabView.swift
@@ -4,14 +4,29 @@ struct MainTabView: View {
     var body: some View {
         TabView {
             FeedView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
             
             SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
             
             UploadPostView()
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
             
             NotificationView()
+                .tabItem {
+                    Image(systemName: "heart")
+                }
             
             ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                }
         }
     }
 }
