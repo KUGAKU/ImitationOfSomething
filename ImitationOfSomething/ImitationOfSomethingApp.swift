40b4212ef0f7749f0ea69688890c5a0b26acc637
gaku yamamoto <githubgemini1124@gmail.com>
2021-03-07 13:54:26 +0900

    first commit

---
+++ ImitationOfSomething/ImitationOfSomethingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImitationOfSomethingApp.swift
+//  ImitationOfSomething
+//
+//  Created by 山本学 on 2021/03/07.
+//
+
+import SwiftUI
+
+@main
+struct ImitationOfSomethingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
